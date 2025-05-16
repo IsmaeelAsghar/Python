03bab76a407279316502e37593afa1c2724d21e4
IsmaeelAsghar <ismaeelasghar96@gmail.com>
2025-05-16 18:35:20 +0500

    Type Casting

---
+++ casting.py
@@ -0,0 +1,18 @@
+# Type Casting
+
+# Integers
+x = int(20)
+y = int(2.0)
+z = int("2")
+
+# Floats
+x = float(20)
+y = float(2.2)
+z = float("5")
+
+# Strings
+x = str(1)
+y = str(1.1)
+z = str("10")
+
+
