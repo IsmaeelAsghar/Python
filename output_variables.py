74e398f0fecd966bd75c1e469f50e250e6d45542
IsmaeelAsghar <ismaeelasghar96@gmail.com>
2025-05-16 16:49:10 +0500

    Print Function

---
+++ output_variables.py
@@ -0,0 +1,17 @@
+# Python print() function is used to output variables.
+# In the print function, you output multiple variables seperated by comma,
+
+x = "Python"
+y = "is"
+z = "awesome"
+print(x, y, z)
+
+#Also use + operator 
+print(x + y + z)
+
+# For numbers + character works as mathametical operator
+a = 2
+b = 8
+print(a+b)
+
+# cannot combine numbers and strings with + operator
