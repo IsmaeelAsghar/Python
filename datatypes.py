5713dc685e51b39f8276e6cafa4768153f66a681
IsmaeelAsghar <ismaeelasghar96@gmail.com>
2025-05-16 17:53:42 +0500

    Data Types

---
+++ datatypes.py
@@ -0,0 +1,17 @@
+# Data types
+# variables can store data of different types
+
+x = "Johan" # Str
+x = 20      # int
+x = 2.0     # float
+x = 1j      # complex
+x = ["jahan", "apple", 32]  # list
+x = ("johan", "apple", 32)  # tuple
+x = range(6)                # range
+x = {"name" : "johan", "age" : "30"}    # dict
+x = {"cherry", "banana", "apple"}       # set
+x = frozenset({
+    "apple", "mango", "banana"          # frozenset
+})
+x = True    # bool
+
