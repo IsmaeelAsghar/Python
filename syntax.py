7cf368bcc35e312f0cd08013e8fbdf33ae235c0a
IsmaeelAsghar <ismaeelasghar96@gmail.com>
2025-05-16 14:20:58 +0500

    This is python syntax

---
+++ syntax.py
@@ -0,0 +1,26 @@
+# Python Indentation
+# Indentation refers to space at beginning of code line.
+# Python use indentaion to indicate the block of code.
+
+if 2 < 5:
+    print("Two is less then Five")
+
+# Use the same number of spaces in same block of code otherwise python will give you error
+
+if 5 > 2:
+    print("Five is greater then Two")
+    print("Five is greater then Two")
+
+# Variables 
+x = 5
+print(x)
+
+y = "Hello World"
+print(y)
+
+# comments
+# Use multiline string in triple quotes in your code and place comment inside this
+
+"""this is comment
+written in more then one line
+"""
