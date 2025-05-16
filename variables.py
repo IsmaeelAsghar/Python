a1b4fa887a8ce704b5e71937569ce93ab80baa97
IsmaeelAsghar <ismaeelasghar96@gmail.com>
2025-05-16 15:46:44 +0500

    variables.py

---
+++ variables.py
@@ -0,0 +1,36 @@
+# python has no command for declaring a variable.
+# variable created a moment assign the value to it.
+# Creating variables
+a = 32
+b = "Hello wolrd!"
+
+print(a)
+print(b)
+
+x = 4 # x is int type
+x = "salary" # now x is str type
+print(x)
+
+# Type casting
+x = str(3)
+y = float(3)
+z = int(3)
+
+print(x)
+print(y)
+print(z)
+
+#Get Type
+x = 3.5
+print(type(x))
+
+# string variables can be declared by using single or double quotes
+
+a = "johan" 
+# same as
+a = 'johan'  
+
+# Variable names are case sensitive
+a = "Johan"
+A = 35
+# This will create two variables. A will not overrite a.
