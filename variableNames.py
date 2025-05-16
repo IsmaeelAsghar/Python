ffc9b50d88235b101ab8f81c42ef7d2ef998222e
IsmaeelAsghar <ismaeelasghar96@gmail.com>
2025-05-16 16:08:18 +0500

    variable Names

---
+++ variableNames.py
@@ -0,0 +1,35 @@
+# variable have short names but meanigfull.
+# variable name must start with letter or underscore character
+# variable cannot start wirh numbers
+# variable cannot start with python keywords
+# variable names are case sensitive like(age, Age, AGE are thre different variables)
+
+# legal variable names
+
+carName = "City"
+CarName = "Corola"
+car_name = "Camery"
+car2 = "Crown"
+_carname = "Civic"
+CARNAME = "civic"
+
+# Illegal variable names:
+2car = "corola"
+car name = "city"
+car- name = "Crown"
+
+# Multi word variables names
+# variable name are more the one word are difficult to read. we use some teqniques
+
+# Camel case
+# Each word start with capital letter accepts first
+
+carBrandName = "Honda"
+
+# Pascal Case
+# Each word start with capital letter
+CarBrandName = "Toyota"
+
+# Snake Case
+# Each word seperated with underscore character
+car_brand_name = "Suzuki"
