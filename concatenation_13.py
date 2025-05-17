8134b134401ce507261dffb793312f797662e894
IsmaeelAsghar <ismaeelasghar96@gmail.com>
2025-05-17 21:16:13 +0500

    String Concatenation

---
+++ concatenation_13.py
@@ -0,0 +1,14 @@
+# String Concatenation
+# To concatenate two strings use + operator.
+
+a = "Hello"
+b = "World"
+c = a + b
+print(c)
+
+# To add a space a " ":
+a = "Hello"
+b = "World"
+c = a + " " + b
+print(c)
+
