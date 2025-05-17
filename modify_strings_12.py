3a70f6b31d2ed5df00f2f4af6f581bba39cb7fd6
IsmaeelAsghar <ismaeelasghar96@gmail.com>
2025-05-17 14:22:27 +0500

    Modify Strings

---
+++ modify_strings_12.py
@@ -0,0 +1,19 @@
+# Modify Strings
+
+# Upper Case
+a = "Hello, World!"
+print(a.upper())
+
+# Lower Case
+print(a.lower())
+
+# Remove Whitespace
+print(a.strip())
+
+# Replace String
+print(a.replace('H', 'J'))
+
+# Split String
+# The split() method splits the string into substrings if it finds instances of the separator
+print(a.split(","))
+
