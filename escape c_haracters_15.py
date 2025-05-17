dbbdf742871393f8a61967ee561e5f56f464ad95
IsmaeelAsghar <ismaeelasghar96@gmail.com>
2025-05-17 21:34:06 +0500

    Escape Characters

---
+++ escape c_haracters_15.py
@@ -0,0 +1,20 @@
+# Escape Characters
+# To insert characters that are illegal in a string
+
+# txt = "We are the so-called "Vikings" from the north." 
+
+# To fix this problem, use the escape character \"
+txt = "We are the so-called \"vikings\" from the north."
+print(txt)
+
+txt = "We are the so-called 'Vikings' from the north."
+print(txt)
+
+# tab
+txt = "We are the so-called \t from the north."
+print(txt)
+
+# new line
+txt = "We are the so-called \n from the north."
+print(txt)
+
