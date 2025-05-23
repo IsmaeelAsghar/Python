705fc42754d8fb20a41bc1694d14b1b09c17d549
IsmaeelAsghar <ismaeelasghar96@gmail.com>
2025-05-23 12:17:23 +0500

    Operators

---
+++ operators_18.py
@@ -0,0 +1,63 @@
+# Operators
+# Operators are used to perform operations on variables and values
+# Athimetic Operators
+
+# Arithmetic operators are used with numeric values to perform common mathematical operations:
+
+# Addition
+x = 5
+y = 10
+print(x + y)
+
+# Subtraction
+a = 2
+b = 12
+print(a - b)
+
+# Multiplecation
+a = 4
+b = 6
+print(a + b)
+
+# Division
+a = 11
+b = 2
+print(a / b)
+
+# Modulus
+a = 11
+b = 3
+print(a % b)
+
+# Cycling through a list
+list = ['a', 'b', 'c']
+index = 5
+print(list[index % len(list)])
+
+# Exponentiation
+a = 2
+b = 5
+print(a ** b)
+
+# Floor division
+a = 11
+b = 2
+print(a // b)
+
+# Assignment Operators
+# +=
+# -=
+# *=
+# /=
+# %=
+# //= 
+# **=
+
+# Comparison Operators
+# ==	Equal	
+# !=	Not equal	
+# >	Greater than	
+# <	Less than	
+# >=	Greater than or equal to
+# <=	Less than or equal to
+
