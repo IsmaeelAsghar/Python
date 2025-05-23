5551c6f9f2924ec4626f040bda14b40b6ab861a6
IsmaeelAsghar <ismaeelasghar96@gmail.com>
2025-05-23 15:22:45 +0500

    Add list Items

---
+++ add_list_items_22.py
@@ -0,0 +1,19 @@
+# Add list items
+# To add an item to the end of the list, use the append() method:
+list = ["apple", "banana", "cherry"]
+list.append("orange")
+print(list)
+
+# Insert Items
+# The insert() method inserts an item at the specified index
+list = ["apple", "banana", "cherry"]
+list.insert(1, "orange")
+print(list)
+
+# Extend List
+# use extend() menthod to add elements of two lists
+list = ["apple", "banana", "cherry"]
+tropical = ["mango", "pineapple", "papaya"]
+list.extend(tropical)
+print(list)
+
