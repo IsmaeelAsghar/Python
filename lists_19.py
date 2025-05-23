6d44cd085b9418152e14435fe1a7e0a7abedb28b
IsmaeelAsghar <ismaeelasghar96@gmail.com>
2025-05-23 12:41:56 +0500

    Lists

---
+++ lists_19.py
@@ -0,0 +1,33 @@
+# Lists in Python
+# Lists are used to store multiple items in a single variable.
+# Lists are created using square brackets
+
+list = ['apple', 'banana', 'cherry']
+print(list)
+
+mylist = ["apple", "banana", "cherry"]
+print(type(mylist))
+
+# List items are ordered, changeable, and allow duplicate values.
+# it means that the items have a defined order, and that order will not change.
+
+# Changeable
+# The list is changeable, meaning that we can change, add, and remove items in a list after it has been created.
+
+# List lenght
+thislist = ["apple", "banana", "cherry"]
+print(len(thislist))
+
+# The list() Constructor
+# It is also possible to use the list() constructor when creating a new list.
+
+# Python Collections
+# list
+# tuple
+# set
+# dict
+
+# List is a collection which is ordered and changeable. Allows duplicate members.
+# Tuple is a collection which is ordered and unchangeable. Allows duplicate members.
+# Set is a collection which is unordered, unchangeable*, and unindexed. No duplicate members.
+# Dictionary is a collection which is ordered** and changeable. No duplicate members.
