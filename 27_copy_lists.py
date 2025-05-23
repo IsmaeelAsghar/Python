ee0d3ec10dd1b52ebd6da80f48f866df635e1937
IsmaeelAsghar <ismaeelasghar96@gmail.com>
2025-05-23 17:46:30 +0500

    Copy List

---
+++ 27_copy_lists.py
@@ -0,0 +1,17 @@
+# Copy Lists
+# Use the copy() method
+
+thislist = ["apple", "banana", "cherry"]
+list1 = thislist.copy()
+print(list1)
+
+# Use the list() method
+tlist = ["apple", "banana", "cherry"]
+mylist = list(tlist)
+print(mylist)
+
+# Use the slice Operator
+thislist = ["apple", "banana", "cherry"]
+mylist2 = thislist[:]
+print(mylist2)
+
