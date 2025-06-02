69278659ae447e3504bb21564b1b961c41c87ffd
IsmaeelAsghar <ismaeelasghar96@gmail.com>
2025-06-02 10:30:05 +0500

    Update Tuples

--- 31_update_tuples.py
+++ 31_update_tuples.py
@@ -1,38 +1,38 @@
-# # Update Tuples
-# # Once a tuple is created, you cannot change its values.
-# # convert the tuple into a list, change the list, and convert the list back into a tuple.
+# Update Tuples
+# Once a tuple is created, you cannot change its values.
+# convert the tuple into a list, change the list, and convert the list back into a tuple.
 
-# x = ("apple", "banana", "cherry")
-# print(x)
-# y = list(x)
-# print(y)
+x = ("apple", "banana", "cherry")
+print(x)
+y = list(x)
+print(y)
 
-# y[1] = "kivi"
-# print(y)
+y[1] = "kivi"
+print(y)
 
-# x = tuple(y)
-# print(x)
+x = tuple(y)
+print(x)
 
-# # Add Items
-# # Convert the tuple into a list, add "orange", and convert it back into a tuple
-# thistuple = ("apple", "banana", "cherry")
-# print(thistuple)
+# Add Items
+# Convert the tuple into a list, add "orange", and convert it back into a tuple
+thistuple = ("apple", "banana", "cherry")
+print(thistuple)
 
-# y = list(thistuple)
-# print(y)
+y = list(thistuple)
+print(y)
 
-# y.append("orrange")
-# print(y)
+y.append("orrange")
+print(y)
 
-# thistuple = tuple(y)
-# print(thistuple)
+thistuple = tuple(y)
+print(thistuple)
 
-# # Add tuple to a tuple
-# # Create a new tuple with the value "orange", and add that tuple
-# thistuple = ("apple", "banana", "cherry")
-# y = ("orange",)
-# thistuple += y
-# print(thistuple)
+# Add tuple to a tuple
+# Create a new tuple with the value "orange", and add that tuple
+thistuple = ("apple", "banana", "cherry")
+y = ("orange",)
+thistuple += y
+print(thistuple)
 
 
 # Remove Items
